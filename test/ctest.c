cdc398cc70b6b4de8b785b9df83efe09b74f917c
Skrades <zelpos90@gmail.com>
2023-04-21 18:02:41 +0700

    Add calculation tests

--- test/ctest.c
+++ test/ctest.c
@@ -6,10 +6,31 @@
 
 #define _USE_MATH_DEFINES
 
-CTEST(INPUT_FILE, CHECK_CIRCLE_AREA)
+CTEST(Calculation, Check_circle_area)
 {
     Circle circle = {.radius = 2};
     float result = area(circle.radius);
-    float expected = 12.56;
-    ASSERT_DBL_NEAR_TOL(expected, result, 0.01);
+    float expected = M_PI * pow(circle.radius, 2);
+    ASSERT_DBL_NEAR(expected, result);
+}
+
+CTEST(Calculation, Check_circle_perimeter)
+{
+    Circle circle = {.radius = 2};
+    float result = perimeter(circle.radius);
+    float expected = 2 * M_PI * circle.radius;
+    ASSERT_DBL_NEAR(expected, result);
+}
+
+CTEST(Calculation, Check_circle_intersection)
+{
+    Circle circle1 = {.point = {.x = 1.0, .y = 1.0}, .radius = 1};
+    Circle circle2 = {.point = {.x = 2.0, .y = 2.0}, .radius = 3.5};
+    Circle circle3 = {.point = {.x = 5.0, .y = 2.0}, .radius = 3};
+    float result1 = intersect(circle1, circle2);
+    int expected1 = 1;
+    ASSERT_EQUAL(expected1, result1);
+    float result2 = intersect(circle1, circle3);
+    int expected2 = 0;
+    ASSERT_EQUAL(expected2, result2);
 }
