227b4d8131e689cad7936310b84e489dac16782f
Skrades <zelpos90@gmail.com>
2023-04-23 17:25:50 +0700

    Modify geometry.c

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -8,25 +8,20 @@ int main()
 {
     FILE* open;
     int n = 0;
-    int flag;
+    char* circle = "circle";
     char text[80];
     if ((open = fopen("text.txt", "r")) == NULL) {
         printf("Error: can't open the file");
         return 1;
     }
     while (fgets(text, 80, open) != NULL) {
-        flag = Error_check(text);
-        if (flag == 0) {
-            n++;
-        } else {
-            break;
-        }
+        n++;
     }
     Circle* circles = malloc(n * sizeof(Circle));
     rewind(open);
     int i = 0;
-    if (flag == 0) {
-        while (fgets(text, 80, open) != NULL) {
+    while (fgets(text, 80, open) != NULL) {
+        if (Error_check(text) == 0) {
             strcpy(circles[i].n, text);
             float* p = find_numbers(text, 3);
             circles[i].point.x = p[0];
@@ -34,17 +29,20 @@ int main()
             circles[i].radius = p[2];
             circles[i].perimeter = perimeter(circles[i].radius);
             circles[i].area = area(circles[i].radius);
-            i = i + 1;
             free(p);
             p = NULL;
         }
-        for (int i = 0; i < n; i++) {
+        i = i + 1;
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++) {
+        if (strstr(circles[i].n, circle)) {
             printf("%d. %s", i + 1, circles[i].n);
             printf("Perimeter = %.2f\n", circles[i].perimeter);
             printf("Area = %.2f\n", circles[i].area);
             printf("Intersects:\n");
             for (int j = 0; j < n; j++) {
-                if (i != j) {
+                if (i != j && strstr(circles[j].n, circle)) {
                     if (intersect(circles[i], circles[j]) == 1) {
                         printf("%d. circle\n", j + 1);
                     }
